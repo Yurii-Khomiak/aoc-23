e819885bb72744ee27f5e4007fb2df27789e56ba
Yurii Khomiak <yurii.khomiak.repos@gmail.com>
2023-12-05 13:36:03 +0200

    feat: day 2

---
+++ 02/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"myprojects/aoc/23/aoclib"
+)
+
+var maxCubeCounts = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
+
+func CalculatePart1(lines []string) int {
+	res := 0
+	for i, line := range lines {
+		gameId := i + 1
+		if isPossibleGame(line) {
+			res += gameId
+		}
+	}
+	return res
+}
+
+func isPossibleGame(line string) bool {
+	for color, count := range parseMaxColorCounts(line) {
+		if count > maxCubeCounts[color] {
+			return false
+		}
+	}
+	return true
+}
+
+func CalculatePart2(lines []string) int {
+	res := 0
+	for _, line := range lines {
+		power := 1
+		for _, count := range parseMaxColorCounts(line) {
+			power *= count
+		}
+		res += power
+	}
+	return res
+}
+
+func parseMaxColorCounts(line string) map[string]int {
+	res := map[string]int{}
+
+	setStartIndex := strings.IndexAny(line, ":") + 2
+	sets := strings.Split(line[setStartIndex:], ";")
+
+	for _, set := range sets {
+		cubes := strings.Split(strings.Trim(set, " "), ",")
+
+		for _, cube := range cubes {
+			parsed := strings.Fields(strings.Trim(cube, " "))
+
+			count, _ := strconv.ParseInt(parsed[0], 10, 0)
+			color := parsed[1]
+
+			if v, ok := res[color]; !ok || v < int(count) {
+				res[color] = int(count)
+			}
+		}
+	}
+
+	return res
+}
+
+func main() {
+	inputFilepath := os.Args[1]
+
+	lines, err := aoclib.ReadFile(inputFilepath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(CalculatePart1(lines))
+	fmt.Println(CalculatePart2(lines))
+}
---
+++ 02/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	inputLines := []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
+	expected := 8
+
+	if v := CalculatePart1(inputLines); v != expected {
+		t.Fatalf("Expected: %d\nGot: %d", expected, v)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	inputLines := []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
+	expected := 2286
+
+	if v := CalculatePart2(inputLines); v != expected {
+		t.Fatalf("Expected: %d\nGot: %d", expected, v)
+	}
+}
